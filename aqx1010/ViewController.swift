1ab44a99149da4c5b118256f1bde8c70038425cd
Baliga Lab <baligalab@druid.local>
2015-11-13 14:47:50 -0800

    Initial Commit

---
+++ aqx1010/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aqx1010
+//
+//  Created by Baliga Lab on 11/13/15.
+//  Copyright © 2015 Baliga Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
