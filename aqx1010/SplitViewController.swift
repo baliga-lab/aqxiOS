c14f8ce436ec99979146f0299da707f0a878160b
Baliga Lab <baligalab@druid.systemsbiology.net>
2015-11-24 13:50:56 -0800

    connected details view

--- aqx1010/SplitViewController.swift
+++ aqx1010/SplitViewController.swift
@@ -57,7 +57,9 @@ class MyTableViewController: UITableViewController {
                     self.systems.addObject(AqxSystemInfo(aUID: system["uid"] as! String,
                         aName: system["name"] as! String, aURL: system["thumb_url"] as! String))
                 }
-                self.tableView.reloadData()
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableView.reloadData()
+                })
             } catch {
             }
         }
@@ -92,7 +94,48 @@ class MyTableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         print("tapped \(indexPath.row)")
-        let controller = storyboard?.instantiateViewControllerWithIdentifier("DetailView")
-        self.showDetailViewController(controller!, sender: self)
+        if (self.splitViewController!.collapsed) {
+            let controller = storyboard?.instantiateViewControllerWithIdentifier("DetailView")
+            let detailController = controller as! AqxSystemDetailViewController
+            detailController.uid = (systems[indexPath.row] as! AqxSystemInfo).uid
+            self.showDetailViewController(controller!, sender: self)
+        } else {
+            let detailView = splitViewController?.viewControllers[1]
+            // TODO
+        }
+    }
+}
+
+class AqxSystemDetailViewController : UIViewController {
+    var uid: String = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let authToken = NSUserDefaults.standardUserDefaults().objectForKey("GoogleAuthToken") as! String
+        let url = NSURL(string: "https://aquaponics.systemsbiology.net/api/v1.0/system/" + uid)  // TODO: add uid
+        print(url)
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let authString = "Bearer \(authToken)"
+        config.HTTPAdditionalHeaders = ["Authorization": authString]
+        let session = NSURLSession(configuration: config)
+        let task = session.dataTaskWithURL(url!) {(data, response, error) in
+            let s = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print(s)
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(s!.dataUsingEncoding(NSUTF8StringEncoding)!, options: NSJSONReadingOptions.MutableContainers)
+                let details = json as! NSDictionary
+                let sysname = (details["system_details"] as! NSDictionary)["name"] as! String
+                let technique = (details["system_details"] as! NSDictionary)["aqx_technique"] as! String
+                dispatch_async(dispatch_get_main_queue(), {
+                    let systemNameView = self.view.viewWithTag(4711) as! UITextView
+                    systemNameView.text = sysname
+                    let techniqueView = self.view.viewWithTag(4712) as! UITextView
+                    techniqueView.text = technique
+                })
+            } catch {
+            }
+        }
+        task.resume()
+        
     }
 }
